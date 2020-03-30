439d5d873c8f4f4146743f973e699ca96c312dd1
Isaac Colls <colls_isaac@hotmail.com>
2020-03-30 12:31:52 -0300

    project working

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// import css
+import css from './styles/style.styl';
+
+render(<h1>Testing 420!👽</h1>, document.getElementById('root'));
