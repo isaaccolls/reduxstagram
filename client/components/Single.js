9a914476eee98e96e44cfadb2968127073de1379
Isaac Colls <colls_isaac@hotmail.com>
2020-04-02 17:45:46 -0300

    display, add and remove comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -4,16 +4,18 @@ import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params;
     // index of the post
-    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const i = this.props.posts.findIndex((post) => post.code === postId);
     console.log("index of the post:", i);
     // get us the post
     const post = this.props.posts[i];
     console.log("...and the post is: ", post);
+    const postComments = this.props.comments[postId] || [];
     return (
       <div className="single-photo">
         <Photo {...this.props} i={i} post={post} />
-        <Comments />
+        <Comments {...this.props} postComments={postComments} />
       </div>
     );
   }
